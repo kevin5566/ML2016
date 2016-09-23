9bf8cc7c64ba4b02bbd06613ab7d1748a7e3b980
kevin5566 <b00611006@ntu.edu.tw>
2016-09-23 14:43:49 +0800

    ~

---
+++ hw0/q1.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+import numpy as np
+import sys
+
+col=int(sys.argv[1])
+f=open(sys.argv[2],'r')
+
+ans=[]
+for line in f:
+    tmp=line.strip().split(' ')
+    ans.append(float(tmp[col]))
+f.close()
+
+ans.sort()
+ans.reverse()
+strg=""
+i=len(ans)
+for j in range(0,i):
+    strg=strg+str(ans.pop())+","
+print(strg[:-1])
