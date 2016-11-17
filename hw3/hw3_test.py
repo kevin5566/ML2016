c25ad4887d58dc045a2fb99213fdd9360e00a0cb
Kevin Cheng <r05921075@ntu.edu.tw>
2016-11-17 21:09:40 +0800

    r
    f

---
+++ hw3/hw3_test.py
@@ -0,0 +1,15 @@
+import pickle
+import numpy as np
+import sys
+import csv
+
+test=pickle.load(open(sys.argv[1]+'/test.p','r'))
+ID = test['ID']
+
+file=open(sys.argv[3],'w')
+csv.writer(file).writerow(['ID','class'])
+for i in range(0,10000):
+    csv.writer(file).writerow([ID[i],'0'])
+file.close()
+
+
